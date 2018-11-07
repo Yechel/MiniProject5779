ee082ffa6ac61a400c8d2536b718601a63b6489d
yechiel chabany <Yechel@users.noreply.github.com>
2018-11-07 15:24:16 +0200

    Initial commit

---
+++ app/src/main/java/com/natanandyechiel/miniproject5779/MainActivity.java
@@ -0,0 +1,15 @@
+package com.natanandyechiel.miniproject5779;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final android.R.attr R = new android.R.attr();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
